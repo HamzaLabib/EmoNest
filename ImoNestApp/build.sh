1fd835e199cf681eabc57836601b52897fec86f8
HamzaLabib <hamzah.labib@gmail.com>
2025-07-15 01:51:35 -0400

    Update build.sh

--- ImoNestApp/build.sh
+++ ImoNestApp/build.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+set -e
+
+echo "🚀 Installing dependencies with legacy-peer-deps"
 npm install --legacy-peer-deps
+
+echo "📦 Building web version with expo export:web"
 npx expo export:web
