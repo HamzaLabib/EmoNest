e44aa6be2df98685cb6d5bc340745bbfd60886e5
HamzaLabib <hamzah.labib@gmail.com>
2025-07-15 16:05:36 -0400

    Reconfigured project setup
    resolved Expo dependency issues, clean cache, and reconfigure project setup

--- ImoNestApp/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "🚀 Installing dependencies with legacy-peer-deps"
-npm install --legacy-peer-deps
-
-echo "📦 Building web version with expo export:web"
-npx expo export:web
